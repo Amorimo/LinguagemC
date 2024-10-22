04014eb37a36caf18d8408c55ba7ae808083af72
Matheus Amorim <matheus.a.amorim10@gmail.com>
2024-10-22 17:25:31 -0300

    Criação

---
+++ src/boasvindas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Adicionar um pequeno cabeçalho
+    printf ("-----------------------Progama mensagem-----------------------");
+    printf ("\nSeja Bem vindo ao nosso progama\n");
+    printf ("-----------------------Fim-----------------------\n");
+    return 0;
+}
